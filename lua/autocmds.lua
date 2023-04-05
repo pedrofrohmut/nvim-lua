5d1b8f52919424bd0d15982c06a4e76ceaca8d43
Pedro Frohmut <pedrofrohmut@gmail.com>
2023-04-05 11:17:38 -0300

    augroups for autocmds

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -1,20 +1,29 @@
 local autocmd = vim.api.nvim_create_autocmd
 local augroup = vim.api.nvim_create_augroup
 
+-- Groups
+augroup("FileTypeCmds", { clear = true })
+augroup("CursorLine", { clear = true })
+
 -- Remove auto insert comments next line
-autocmd("FileType", { pattern = "*", command = "setlocal formatoptions-=cro" })
+autocmd("FileType", {
+    group = "FileTypeCmds", pattern = "*", command = "setlocal formatoptions-=cro" })
 
 -- Set 4 indent to all files
-autocmd("FileType", { pattern = "*", command = "setlocal shiftwidth=4" })
+autocmd("FileType", {
+    group = "FileTypeCmds", pattern = "*", command = "setlocal shiftwidth=4" })
 
--- Only show the cursor line in the active buffer.
-augroup("CursorLine", { clear = true })
--- CursorLine Begin
+-- Set 2 for exceptions
+autocmd("FileType", {
+    group = "FileTypeCmds", pattern = {"html"}, command = "setlocal shiftwidth=2 "})
+
+-- Set cursorline on enter
 autocmd({ "VimEnter", "WinEnter", "BufWinEnter" }, {
     group = "CursorLine", pattern = "*", command = "setlocal cursorline" })
+
+-- Set nocursorline on leave
 autocmd("WinLeave", {
     group = "CursorLine", pattern = "*", command = "setlocal nocursorline" })
--- CursorLine End
 
 -- Remove Trailing White Spaces onSave (And dont show errors)
 autocmd("BufWritePre", { pattern = "*", command= ":%s/\\s\\+$//e" })
